15660d9d9facec5b2a43236282f282f38e55f774
Kotova Dana <44918208+kotovadana@users.noreply.github.com>
2018-11-25 10:26:12 +0200

    my second commit and PUSH

--- src/com.Kotova.dataSecondTry/firstClassSecondTry.java
+++ src/com.Kotova.dataSecondTry/firstClassSecondTry.java
@@ -1,4 +1,18 @@
 package com.Kotova.dataSecondTry;
 
 public class firstClassSecondTry {
+
+    private String SecondTry;
+
+    public firstClassSecondTry() {
+
+    }
+
+    public String getSecondTry() {
+        return SecondTry;
+    }
+
+    public void setSecondTry(String secondTry) {
+        SecondTry = secondTry;
+    }
 }
